44d9463da9975c313d79d289212bd318d4a4d422
daniel kerr <danielkerr707@gmail.com>
2022-10-09 22:24:52 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,6 +1,15 @@
 module "network" {
   source  = "app.terraform.io/hashicat-azure677/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "myresourcegroup"
   # insert required variables here
+}
+
+
+resource "azurerm_resource_group" "myresourcegroup" {
+  name     = "${var.prefix}-workshop"
+  location = var.location
+
+  tags = {
+    environment = "Production"
+  }
 }
